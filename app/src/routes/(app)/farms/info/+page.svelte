34fb6fa1e550437b9fe0115d2db4294c372f5968
triesap <triesap@radroots.dev>
2026-01-18 17:24:20 +0000

    chore: initialize rust workspace

--- app/src/routes/(app)/farms/info/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import { ls } from "$lib/utils/i18n";
-</script>
-
-<div class={`flex flex-col w-full justify-start items-start`}>
-    <p class={`font-sans font-[400] text-base text-ly0-gl`}>
-        {`${$ls(`icu.*_information`, { value: `${$ls(`common.farm`)}` })}`}
-    </p>
-</div>
