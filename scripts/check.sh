1d060a7d95aa29300d64dde9fe25522e2ab4f176
triesap <tyson@radroots.org>
2026-04-21 20:04:30 +0000

    app: close pack day print host lane

--- scripts/check.sh
+++ scripts/check.sh
@@ -8,7 +8,8 @@ cd "${repo_root}"
 cargo metadata --format-version 1 --no-deps
 cargo test -p radroots_studio_app_i18n english_pack_day_copy_matches_the_contextual_execution_contract
 cargo test -p radroots_studio_app source_guards
-cargo test -p radroots_studio_app runtime_export_pack_day
+cargo test -p radroots_studio_app pack_day_host_handoff
+cargo test -p radroots_studio_app pack_day
 cargo check -p radroots_studio_app
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
